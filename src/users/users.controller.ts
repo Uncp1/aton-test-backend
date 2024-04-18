fdbdd7e4ea1bb1d1c962eeac72a8e0b2d2085fae
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-04-18 10:23:13 +0300

    readme update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.schema';
 import { CreateUserDto } from './create-user.dto';
+import { JwtGuard } from 'src/guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtGuard)
   @Get()
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
